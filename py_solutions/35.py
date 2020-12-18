304f9be97b8dbe039ce1f70355542816ef59e929
Nikhil R <osregularcoder@gmail.com>
2020-12-18 12:10:20 +0530

    Nikhil: Working for all except one case

--- py_solutions/35.py
+++ py_solutions/35.py
@@ -4,38 +4,54 @@ import re
 
 lines = [l.rstrip('\n') for l in sys.stdin]
 
-def handleExpression(line, prev):
-    print("processsing", line)
+def handleExpression(line, prev, prevExtraOperator):
+    print("processsing", line, prev, prevExtraOperator)
 
     if line == "":
-        return 0
+        return (prev, "")
+    elif line == " + ":
+        return (prev, "+")
+    elif line == " * ":
+        return (prev, "*")
 
+    if prevExtraOperator != "":
+        print("changing line to", line + " " + prevExtraOperator + " " + prev.__str__())
+        line = (line + " " + prevExtraOperator + " " + prev.__str__())
+    
     elements = line.split(" ")
 
-    print elements
-    result = int(elements[0])
-    operator = ""
+    result = prev
+    if elements[0] != "":
+        result = int(elements[0])
+    
+    operator = prevExtraOperator
+    print elements[1:]
     for el in elements[1:]:
         if el == "*" or el == "+":
             operator = el
         else:
-            num = prev
             if el != "":
                 num = int(el)
+            else:
+                break
 
             if operator == "*":
                 result = result * num
+                operator = ""
             else:
                 result = result + num
-    return result
+                operator = ""
+
+    return (result, operator,)
 
 for i, l in enumerate(lines):
     expressions = re.split("\(|\)", l)
-    expressions.reverse()
+    # expressions.reverse()
 
     prev = 0
+    extraOperator = ""
     for subEx in expressions:
-        result = handleExpression(subEx, prev)
-        prev = result
-        print result
-    # print result
+        (prev, extraOperator) = handleExpression(subEx, prev, extraOperator)
+        print("returned", prev, extraOperator)
+        print
+    print prev
