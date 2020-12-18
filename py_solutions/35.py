3392275cdb6277546b9f2065f788d0b9dda31fd9
Nikhil R <osregularcoder@gmail.com>
2020-12-18 11:09:51 +0530

    Nikhil: Parse most expressions successfully

--- py_solutions/35.py
+++ py_solutions/35.py
@@ -4,21 +4,38 @@ import re
 
 lines = [l.rstrip('\n') for l in sys.stdin]
 
-def handleExpression(line):
+def handleExpression(line, prev):
+    print("processsing", line)
+
+    if line == "":
+        return 0
+
     elements = line.split(" ")
 
+    print elements
     result = int(elements[0])
     operator = ""
     for el in elements[1:]:
         if el == "*" or el == "+":
             operator = el
         else:
+            num = prev
+            if el != "":
+                num = int(el)
+
             if operator == "*":
-                result = result * int(el)
+                result = result * num
             else:
-                result = result + int(el)
+                result = result + num
     return result
 
 for i, l in enumerate(lines):
-    result = handleExpression(l)
-    print result
+    expressions = re.split("\(|\)", l)
+    expressions.reverse()
+
+    prev = 0
+    for subEx in expressions:
+        result = handleExpression(subEx, prev)
+        prev = result
+        print result
+    # print result
