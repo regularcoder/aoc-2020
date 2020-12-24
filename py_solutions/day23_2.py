43e1a5678b976ce864d1033aaacb8f728757b397
Nikhil R <osregularcoder@gmail.com>
2020-12-24 21:12:03 +0530

    Nikhil: Finish day 23 part 2

--- py_solutions/day23_2.py
+++ py_solutions/day23_2.py
@@ -4,56 +4,82 @@ import re
 import time
 from collections import deque 
 
-input = "389125467"
-cups = deque([int(cupStr) for cupStr in input])
+input = "156794823"
+
+def buildCups(input):
+    cupsList = [int(cupStr) for cupStr in input]
+
+    cups = {}
+    for i, n in enumerate(cupsList):
+        next = (i + 1) % len(cupsList)
+
+        cups[n] = cupsList[next]
+
+    return (cupsList[0], cups)
+
+def printCups(cups, current):
+    pointer = current
+    counter = 0
+
+    cupList = [current]
+    while True:
+        cupList.append(cups[pointer])
+        pointer = cups[pointer]
+
+        if pointer == current:
+            break
+
+    print(cupList)
 
 moveCount = 1
-def makeMove(cups):
+def makeMove(cups, current):
     global moveCount
-    print("\n-- move %d --" % moveCount)
-    print("cups", cups)
+    # print("\n-- move %d --" % moveCount)
+    # printCups(cups, current)
 
-    current = cups.popleft()
+    # print("current cup %d" % current) 
 
-    print("current cup %d" % current) 
-
-    pickedUp = [cups.popleft(), cups.popleft(), cups.popleft()] 
-    print("pick up %s" % pickedUp)
-
-    cups.appendleft(current)
-    destination = current - 1
-    if destination == 0:
-        destination = len(cups) - 1
-    while destination in pickedUp:
-        # print("subsr %d" % destination)
-        destination = destination - 1
-        if destination == 0:
-            destination = len(cups) - 1
-    
-    # print("destination %d" % destination)
-
-    # destinationIndex = current - 1
+    # pick up 3 and remove them
+    next3 = []
+    pointer = current
     for i in range(3):
-        cups.insert(destination + i, pickedUp[i])
+        pointer = cups[pointer]
+        next3.append(pointer)
     
+    cups[current] = cups[pointer]
+
+    # print("picked up", next3)
+
+    # decide destination
+    destination = current - 1 or len(cups)
+    while destination in next3:
+        destination = destination - 1 or len(cups)
+    
+    # print("destination", destination)
+
+    # add picked up after destination
+    (cups[destination], cups[next3[2]]) = (next3[0], cups[destination])
+
     moveCount += 1
 
-    # cups.append(cups.popleft())
-    cups.rotate(-1)
-
-    # print(cups)
-
-# for i in range(10, 10000):
-#     cups.append(i)
+    return (cups, cups[current])
 
 tic = time.perf_counter()
-for i in range(10):
-    makeMove(cups)
+(current, cups) = buildCups(input)
+
+# add extra elements
+lastPointer = int(input[-1])
+for i in range(10, 1000001):
+    cups[lastPointer] = i
+    lastPointer = i
+cups[lastPointer] = current
+
+for i in range(10000000):
+    (cups, current) = makeMove(cups, current)
 toc = time.perf_counter()
 
-# print(cups)
 print(f"Finished processing in in {toc - tic:0.4f} seconds")
 
-index1 = cups.index(1)
-answer = cups[(index1 + 1) % len(cups)] * cups[(index1 + 2) % len(cups)]
-print("answer %d" % answer)
+next1 = cups[1]
+next2 = cups[next1]
+print ("%d * %d = %d" % (next1, next2, next1 * next2))
